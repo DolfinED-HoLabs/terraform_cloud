a47976d3878d204b4f2cf0d66f311ec975d3860b
DolfinED-HoLabs <e.abousherif@gmail.com>
2024-12-16 11:53:33 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,10 @@ resource "aws_instance" "myec2_Public" {
   ami           = "ami-07caf09b362be10b8"
   instance_type = "t2.nano"
 
+   
+  tags = {
+    Name = "dolfined_demo"
+    Env = "Prod"
+  }
 
 }
