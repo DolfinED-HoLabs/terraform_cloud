470c608e5c101cc429139cd16e64153a6a59c8cd
DolfinED-HoLabs <e.abousherif@gmail.com>
2024-12-16 11:52:09 +0400

    Removed the tags

--- main.tf
+++ main.tf
@@ -6,9 +6,5 @@ resource "aws_instance" "myec2_Public" {
   ami           = "ami-07caf09b362be10b8"
   instance_type = "t2.nano"
 
-   
-  tags = {
-    Name = "dolfined_demo"
-    Env = "Prod"
-  }
+
 }
